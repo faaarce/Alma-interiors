b7ae9d318e1f78dff21e9a6f1d9929d6e69f8ecf
faaarce <risfararie@gmail.com>
2026-03-08 14:31:52 +0700

    fix casesensitive

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import HomePage from "./pages/Homepage";
+import HomePage from "./pages/HomePage";
 import WorkPage from "./components/WorkPage";
 
 
